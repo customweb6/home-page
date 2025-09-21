077b061469e3d5fcec9a13ded4b4407e7426a2af
customweb6 <customweb6@gmail.com>
2025-09-20 17:07:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ def submit_business():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host="0.0.0.0", port=port)
